3a57d2aabc008e274da858fd0a9a12e5df28815d
Daniel151505 <daniel150520011@gmail.com>
2022-10-15 17:16:08 -0500

    Incrementar y Decrementar

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
   <h2>Counter</h2>
   <p> {{ counter }} <sup>2</sup> = {{ squareCounter}} </p>
+
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+
 </template>
 
 <script>
@@ -13,6 +19,12 @@ export default {
   methods: {
     getSquareValue() {
       return this.counter * this.counter
+    },
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
     }
   },
   computed: {
@@ -25,5 +37,21 @@ export default {
 </script>
 
 <style>
+
+  button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+  }
+
+  button:hover {
+    background-color: #5aa67B;
+    transition: 0.3s ease-in-out;
+  }
 
 </style>
