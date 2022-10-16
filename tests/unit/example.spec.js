d24afaaf64bed3e66d39eade14c2d72dc472eba1
Daniel151505 <daniel150520011@gmail.com>
2022-10-16 15:21:00 -0500

    Expect

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,17 +3,14 @@ describe('CounterComponent', () => {
   test('Debe ser mayor a 10', () => {
 
     // Arreglar
-    let value = 20;
+    let value = 10;
 
     // Estimulo
     value = value + 2
 
     // Observar el resultado
-    if( value > 10) {
-      // TODO todo bien!
-    } else {
-      throw `${value} no es mayor a 10`
-    }
+   
+    expect( value ).toBeGreaterThan( 10 )
 
   })
 
