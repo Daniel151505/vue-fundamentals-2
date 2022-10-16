d3bd425b2a768420425b4970215e19ba48aeafd3
Daniel151505 <daniel150520011@gmail.com>
2022-10-16 15:11:04 -0500

    Pruebas

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,20 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('CounterComponent', () => {
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  test('Debe ser mayor a 10', () => {
+
+    // Arreglar
+    let value = 20;
+
+    // Estimulo
+    value = value + 2
+
+    // Observar el resultado
+    if( value > 10) {
+      // TODO todo bien!
+    } else {
+      throw `${value} no es mayor a 10`
+    }
+
   })
+
 })
